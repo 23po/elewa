227706c7419fcf613ef4f9f269d6ac5058449c9d
calvin <ckodalo@gmail.com>
2023-03-09 14:42:37 +0300

    added component

---
+++ libs/elements/layout/modals/src/lib/components/error-prompt-modal/error-prompt-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'italanta-apps-error-prompt-modal',
+  templateUrl: './error-prompt-modal.component.html',
+  styleUrls: ['./error-prompt-modal.component.css']
+})
+export class ErrorPromptModalComponent {
+
+}
--- libs/elements/layout/modals/src/lib/modals.module.ts
+++ libs/elements/layout/modals/src/lib/modals.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ErrorPromptModalComponent } from './components/error-prompt-modal/error-prompt-modal.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    ErrorPromptModalComponent
+  ],
 })
 export class ModalsModule {}
